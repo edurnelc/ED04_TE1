66c4ce02b1fa00154f00de26505aa0aa371b33c2
edurne <eleite@birt.eus>
2021-05-12 18:50:37 +0200

    Version 5. Creacion de la constante COMISON.

--- src/main/java/com/mycompany/ud4_te1/CCuenta.java
+++ src/main/java/com/mycompany/ud4_te1/CCuenta.java
@@ -84,8 +84,9 @@ public class CCuenta {
     public void retirar(double cantidad) throws Exception {
         if (cantidad <= 0)
             throw new Exception ("No se puede retirar una cantidad negativa");
-        if (estado() < cantidad + 2)
+        if (estado() < cantidad + COMISION)
             throw new Exception ("No se hay suficiente saldo");
-        saldo = saldo - (cantidad + 2);
+        saldo = saldo - (cantidad + COMISION);
     }
+    public static final int COMISION = 2;
 }
