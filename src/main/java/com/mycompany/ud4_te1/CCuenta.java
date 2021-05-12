a0ef2784b2c7149e9197ef24f4cd30a7ba36e49d
edurne <eleite@birt.eus>
2021-05-12 19:23:36 +0200

    Version 3.3. Al encapsualr he visto que el metodo estado es lo mismo que getSaldo, por lo que lo he borrado y he renombrado el metod "estado" a "getSaldo".

--- src/main/java/com/mycompany/ud4_te1/CCuenta.java
+++ src/main/java/com/mycompany/ud4_te1/CCuenta.java
@@ -1,5 +1,9 @@
 package com.mycompany.ud4_te1;
 
+/**
+ *
+ * @author Edurne Leite
+ */
 public class CCuenta {
 
     private String nombre;
@@ -8,27 +12,32 @@ public class CCuenta {
     private double tipoInteres;
 
     /**
-     * @return the nombre
+     * El metodo getNombre consulta el atributo nombre de la clase CCuenta y lo devuleve como parametro.
+     * @return nombre, atributo de la clase CCuenta
      */
     public String getNombre() {
         return nombre;
     }
 
     /**
+     * El metodo setNombre modifica el atribtuo nombre de la clase CCuenta
      * @param nombre the nombre to set
+     * 
      */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
     /**
-     * @return the cuenta
+     * El metodo getCuenta consulta el atributo cuenta de la clase CCuenta y lo devuleve como parametro.
+     * @return cuenta, el atributo cuante de la clase CCuenta 
      */
     public String getCuenta() {
         return cuenta;
     }
 
     /**
+     * El metodo setCuenta modifica el atribtuo cuenta de la clase CCuenta
      * @param cuenta the cuenta to set
      */
     public void setCuenta(String cuenta) {
@@ -36,13 +45,7 @@ public class CCuenta {
     }
 
     /**
-     * @return the saldo
-     */
-    public double getSaldo() {
-        return saldo;
-    }
-
-    /**
+     * El metodo setSaldo consulta el atributo saldo de la clase CCuenta y lo devuleve como parametro.
      * @param saldo the saldo to set
      */
     public void setSaldo(double saldo) {
@@ -50,6 +53,7 @@ public class CCuenta {
     }
 
     /**
+     * El metodo getTipoInteres consulta el atributo tipo interes de la clase CCuenta y lo devuleve como parametro.
      * @return the tipoInteres
      */
     public double getTipoInteres() {
@@ -57,23 +61,39 @@ public class CCuenta {
     }
 
     /**
+     * El metodo setTipoInteres consulta el atributo tipo interes de la clase CCuenta y lo devuleve como parametro.
      * @param tipoInteres the tipoInteres to set
      */
     public void setTipoInteres(double tipoInteres) {
         this.tipoInteres = tipoInteres;
     }
 
-
+    /**
+     *
+     * @param nom, atributo nombre de la clase CCuenta
+     * @param cue, atributo cuenta de la clase CCuenta
+     * @param sal, atributo saldo de la clase CCuenta
+     * @param tipo, atributo tipoInteres de la clase CCuenta
+     */
     public CCuenta(String nom, String cue, double sal, double tipo) {
         nombre = nom;
         cuenta = cue;
         saldo = sal;
     }
 
-    public double estado() {
+    /**
+     * El metodo getSaldo consulta el atributo saldo de la clase CCuenta y lo devuleve como parametro.
+     * @return the saldo
+     */
+    public double getSaldo() {
         return saldo;
     }
 
+    /**
+     * El metodo ingresar, ingresa la cantidad pasada como parametro y actualiza el saldo.
+     * @param cantidad, cantidad de dinero a ingresar en la cuenta.
+     * @throws Exception, en caso de que la cantidad de dinero sea una numero negativa
+     */
     public void ingresar(double cantidad) throws Exception {
         if (cantidad < 0) {
             throw new Exception("No se puede ingresar una cantidad negativa");   
@@ -81,12 +101,22 @@ public class CCuenta {
         saldo = saldo + cantidad;
     }
 
+    /**
+     * El metodo retirar, resta la cantidad pasada como parametro y actualiza el saldo.
+     * @param cantidad, cantidad de dinero a retirar de la cuenta.
+     * @throws Exception, si la cantidad de dinero que pasa como parametro es negativa
+     * @throws Exception, si la cantidad de dinero que pasa como parametro es superior a la cantidad de saldo de la cuenta.
+     */
     public void retirar(double cantidad) throws Exception {
         if (cantidad <= 0)
             throw new Exception ("No se puede retirar una cantidad negativa");
-        if (estado() < cantidad + COMISION)
+        if (getSaldo() < cantidad + COMISION)
             throw new Exception ("No se hay suficiente saldo");
         saldo = saldo - (cantidad + COMISION);
     }
+
+    /**
+     * Metodo que crea como constante la cantidad de 2 euros, la comision que cobra el banco.
+     */
     public static final int COMISION = 2;
 }
