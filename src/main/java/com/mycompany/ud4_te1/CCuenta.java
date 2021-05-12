e80286aa7c64f3825ea930a58452031bc746f276
edurne <eleite@birt.eus>
2021-05-12 18:35:56 +0200

    Version 3.2. Encapsulado de los cuatro atributos de la clase CCuenta. .

--- src/main/java/com/mycompany/ud4_te1/CCuenta.java
+++ src/main/java/com/mycompany/ud4_te1/CCuenta.java
@@ -7,6 +7,62 @@ public class CCuenta {
     private double saldo;
     private double tipoInteres;
 
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the cuenta
+     */
+    public String getCuenta() {
+        return cuenta;
+    }
+
+    /**
+     * @param cuenta the cuenta to set
+     */
+    public void setCuenta(String cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    /**
+     * @return the saldo
+     */
+    public double getSaldo() {
+        return saldo;
+    }
+
+    /**
+     * @param saldo the saldo to set
+     */
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    /**
+     * @return the tipoInteres
+     */
+    public double getTipoInteres() {
+        return tipoInteres;
+    }
+
+    /**
+     * @param tipoInteres the tipoInteres to set
+     */
+    public void setTipoInteres(double tipoInteres) {
+        this.tipoInteres = tipoInteres;
+    }
+
 
     public CCuenta(String nom, String cue, double sal, double tipo) {
         nombre = nom;
